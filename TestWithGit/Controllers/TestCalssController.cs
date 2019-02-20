afcee084f80e1903e23b82aace226a5f1643a63d
Alex-Tochilovsky <alex.tochilovsky@gmail.com>
2019-02-20 18:11:27 +0200

    Testing first commint - File changes.

---
+++ TestWithGit/Controllers/TestCalssController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWithGit.Controllers
+{
+    public class TestCalssController
+    {
+        int f1 = 0;
+    }
+}
