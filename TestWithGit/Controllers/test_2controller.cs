3830b472bcc79f925f798b9894e3e5116606f717
Alex-Tochilovsky <alex.tochilovsky@gmail.com>
2019-02-26 15:44:41 +0200

    New controller

---
+++ TestWithGit/Controllers/test_2controller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWithGit.Controllers
+{
+    public class test_2controller
+    {
+        string temp = "alex";
+        int x = 10;
+    }
+}
