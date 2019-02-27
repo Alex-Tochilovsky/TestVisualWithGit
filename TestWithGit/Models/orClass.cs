66a7b970710849befeb18aabaad90bb2b286156a
orbarda5 <46130545+orbarda5@users.noreply.github.com>
2019-02-27 15:44:53 +0200

    הוספתי מחלקה  במודל

---
+++ TestWithGit/Models/orClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWithGit.Models
+{
+    public class orClass
+    {
+        int a = 1;
+        float b = 2;
+        double c = 3;
+        
+    }
+}
